0dc3c93fa88e166fbea6919dde7cb0e48b4d8f4d
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2026-06-25 17:05:04 -0300

    contador funcionando

--- app/src/main/java/com/example/atividades/ContadorCliques.java
+++ app/src/main/java/com/example/atividades/ContadorCliques.java
@@ -1,6 +1,10 @@
 package com.example.atividades;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ContadorCliques extends AppCompatActivity {
 
+    Button btn;
+    TextView tv;
+    int c;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_contador_cliques);
+        btn = findViewById(R.id.botao);
+        tv =findViewById(R.id.textView);
+        btn.setText("Clique");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c++;
+                Button b = (Button) v;
+                tv.setText(String.valueOf(c));
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
